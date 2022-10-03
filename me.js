f5000723105e668d064d018dd2b7ecfae2745a4b
Katie Grace <katiecgrace@gmail.com>
2022-10-03 14:04:46 -0400

    Website Files

---
+++ me.js
@@ -0,0 +1,7 @@
+function showInfo(){
+	// show back to the user, on the <span> element
+	// makes the div visible
+	// element.style can be used to change CSS properties of an HTML document
+	document.getElementById("addy").style.visibility = "visible";
+
+}
